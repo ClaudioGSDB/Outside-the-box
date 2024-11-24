c0e5f7b4495eedfb5dc4ec4170a5610c7b32d072
RoboZero <68201467+RoboZero@users.noreply.github.com>
2024-11-24 02:00:55 -0500

    [LEVEL-1] Add gl dependency (hopefully no conflicts)

--- app/src/main/java/com/example/outsidethebox/levels/Level0.java
+++ app/src/main/java/com/example/outsidethebox/levels/Level0.java
@@ -1,8 +1,10 @@
 // Level0.java
 package com.example.outsidethebox.levels;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -14,9 +16,22 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.outsidethebox.GlobalVariables;
 import com.example.outsidethebox.R;
 
-public class Level0 extends AppCompatActivity {
+import java.util.ArrayList;
+
+import MiniSprite.CustomTransforms.MiniSprite;
+import MiniSprite.MiniSpriteSurface;
+import MiniSprite.Transform2D;
+import MiniSprite.IMiniSpriteSurfaceListener;
+
+public class Level0 extends AppCompatActivity implements IMiniSpriteSurfaceListener {
     private int levelNum;
     private GlobalVariables globalVariables;
+
+    private MiniSpriteSurface miniSpriteSurface;
+    private boolean started;
+    private final double TAU = Math.PI * 2;
+
+    private ArrayList<Transform2D> petals = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,11 @@ public class Level0 extends AppCompatActivity {
         // Initialize GlobalVariables
         globalVariables = GlobalVariables.getInstance();
 
+
+        SurfaceView mySurface = findViewById(R.id.surfaceView);
+        miniSpriteSurface = new MiniSpriteSurface(mySurface);
+        miniSpriteSurface.addListener(this);
+
         // Get current level number
         try {
             levelNum = Integer.parseInt(getClass().getSimpleName().replaceAll("\\D", ""));
@@ -58,6 +78,51 @@ public class Level0 extends AppCompatActivity {
 
         // Finish the activity
         finish();
+    }
+
+    @Override
+    public void onPreUpdate(MiniSpriteSurface miniSpriteSurface, Canvas canvas) {
+        if(started) return;
+
+        int centerX = canvas.getWidth() / 2; // Center of screen
+        int centerY = canvas.getHeight() / 2;
+        double drawableDimensionWidthToHeightRatio = 1 / 2.24; // Petal drawable is 227 w 508 h.
+        int height = (int)(canvas.getWidth() / 2.3); // 2 petals cover horizontal screen.
+        int width = (int) (height * drawableDimensionWidthToHeightRatio);
+        int anchorOffsetY = -height / 2; // Petal's bottom should be at the center.
+
+        int petalCount = 10;
+
+        for(int i = 0; i < petalCount; i++){
+            double angleRads = (TAU / petalCount) * i;
+            double angleDegrees = Math.toDegrees(angleRads);
+            Transform2D petal = new MiniSprite.Builder(R.drawable.level0_petal, getResources())
+                    .withName("Petal")
+                    .withDimensions(width, height)
+                    .withPositionXY(centerX, centerY)
+                    .withAnchorOffsetXY(0, anchorOffsetY)
+                    .withRotationAngleDegrees((int) angleDegrees)
+                    .build();
+
+            petals.add(petal);
+            miniSpriteSurface.AllTransforms.instantiateTransform(petal, 1);
+        }
+
+        started = true;
+    }
+
+    @Override
+    public void onPreDrawSprites(MiniSpriteSurface miniSpriteSurface, Canvas canvas) {
+        canvas.drawColor(getResources().getColor(R.color.black));
+    }
+
+    @Override
+    public void onPostDrawSprites(MiniSpriteSurface miniSpriteSurface, Canvas canvas) {
+        float rotationSpeed = 20;
+
+        for (Transform2D petal : petals) {
+            petal.RotationAngleDegrees += rotationSpeed;
+        }
     }
 }
 
